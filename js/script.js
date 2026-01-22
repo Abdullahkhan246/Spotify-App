0ef386097a1d5dacb4991c66342f4935837994f8
Abdullahkhan246 <anittawilson50@gmail.com>
2026-01-22 08:29:58 +0500

    URL change

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ function secondsToMinutesSeconds(seconds) {
  async function getSongs (folder){
 currFolder = folder;
 
-    let a = await fetch (`http://127.0.0.1:5500/${folder}/`);
+    let a = await fetch (`/${folder}/`);
 let response = await a.text();
 let div = document.createElement("div");
 div.innerHTML = response;
@@ -88,7 +88,7 @@ const playMusic = (track, pause = false) =>{
 
 async function displayAlbums() {
     console.log("displaying albums");
-    let a = await fetch(`http://127.0.0.1:5500/songs/`);
+    let a = await fetch(`/songs/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -105,7 +105,7 @@ async function displayAlbums() {
             let folder = e.href.split("/").slice(-1)[0];
             //meta data of the folder
 
-            let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`);
+            let a = await fetch(`/songs/${folder}/info.json`);
             let response = await a.json();
             cardContainer.innerHTML += `
             <div data-folder="${folder}" class="card">
